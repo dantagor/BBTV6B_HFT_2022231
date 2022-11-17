a78c48e20c0cc28f6de0b76c09f6a2af0574ede9
dantagor <dantagor@gmail.com>
2022-11-17 17:35:14 +0100

    Non-CRUD függvények hozzáadva az ITransactionLogichoz

--- BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Logic/Interfaces/ITransactionLogic.cs
+++ BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Logic/Interfaces/ITransactionLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BBTV6B_HFT_2022231.Models.DTOs;
 using BBTV6B_HFT_2022231.Models.Entities;
 
 namespace BBTV6B_HFT_2022231.Logic.Interfaces
@@ -14,5 +15,9 @@ namespace BBTV6B_HFT_2022231.Logic.Interfaces
         void Delete(int id);
         Transaction Read(int id);
         IQueryable<Transaction> ReadAll();
+
+        Stock BestSellerStock();
+        Exchange MostPopularExchange();
+        IQueryable<TransactionStatistics> ReadTransactionStats();
     }
 }
