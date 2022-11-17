5afc662fc6e5ff400642d70d57aff2ce3c6afad3
dantagor <dantagor@gmail.com>
2022-11-17 17:20:12 +0100

    ITransactionLogic interface létrehozva

---
+++ BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Logic/Interfaces/ITransactionLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BBTV6B_HFT_2022231.Models.Entities;
+
+namespace BBTV6B_HFT_2022231.Logic.Interfaces
+{
+    public interface ITransactionLogic
+    {
+        void Create(Transaction item);
+        void Update(Transaction item);
+        void Delete(int id);
+        Transaction Read(int id);
+        IQueryable<Transaction> ReadAll();
+    }
+}
