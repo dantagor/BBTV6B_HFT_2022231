c3f0252cbaefa940c9296b0e32871bd194901be6
dantagor <dantagor@gmail.com>
2022-11-30 19:16:43 +0100

    Controllers mappa kész

---
+++ BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,53 @@
+﻿using BBTV6B_HFT_2022231.Logic.Interfaces;
+using BBTV6B_HFT_2022231.Models.DTOs;
+using BBTV6B_HFT_2022231.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BBTV6B_HFT_2022231.Endpoint.Controllers
+{
+    public class StatController : ControllerBase
+    {
+        ITransactionLogic transLogic;
+        IStockLogic stockLogic;
+
+        public StatController(ITransactionLogic transLogic, IStockLogic stockLogic)
+        {
+            this.transLogic = transLogic;
+            this.stockLogic = stockLogic;
+        }
+
+        [HttpGet]
+        public Stock HighestDividendStockFromRegion(string region)
+        {
+            return this.stockLogic.HighestDividendStockFromRegion(region); 
+        }
+
+        [HttpGet]
+        public IQueryable<ExchangeStatistics> ReadExchangeStats()
+        {
+            return this.stockLogic.ReadExchangeStats();
+        }
+
+        [HttpGet]
+        public int TotalTransactionsByExchange(string exchange) 
+        {
+            return this.transLogic.TotalTransactionsByExchange(exchange);
+        }
+
+        [HttpGet]
+        public IQueryable<TransactionStatistics> ReadTransactionStats()
+        {
+            return this.transLogic.ReadTransactionStats();
+        }
+
+        [HttpGet]
+        public Stock BestSellerStockByExchange(string exchange)
+        {
+            return this.transLogic.BestSellerStockByExchange(exchange);
+        }
+    }
+}
---
+++ BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Endpoint/Controllers/TransactionController.cs
@@ -0,0 +1,48 @@
+﻿using BBTV6B_HFT_2022231.Logic.Interfaces;
+using BBTV6B_HFT_2022231.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace BBTV6B_HFT_2022231.Endpoint.Controllers
+{
+    public class TransactionController : ControllerBase
+    {
+        ITransactionLogic logic;
+
+        public TransactionController(ITransactionLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Transaction> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Transaction Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Transaction value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Transaction value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
