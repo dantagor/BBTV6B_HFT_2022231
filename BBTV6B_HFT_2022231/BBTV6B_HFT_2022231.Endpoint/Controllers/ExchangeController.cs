49c8c6237088b3251fc1ce2019c0831efb9dd0d3
dantagor <dantagor@gmail.com>
2022-11-30 17:25:57 +0100

    ExchangeController elkészítve

---
+++ BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Endpoint/Controllers/ExchangeController.cs
@@ -0,0 +1,50 @@
+﻿using BBTV6B_HFT_2022231.Logic.Interfaces;
+using BBTV6B_HFT_2022231.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace BBTV6B_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ExchangeController : ControllerBase
+    {
+        IExchangeLogic logic;
+
+        public ExchangeController(IExchangeLogic logic) 
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Exchange> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Exchange Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Exchange value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Exchange value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
--- BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Endpoint/Startup.cs
+++ BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Endpoint/Startup.cs
@@ -2,38 +2,68 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BBTV6B_HFT_2022231.Logic.Classes;
+using BBTV6B_HFT_2022231.Logic.Interfaces;
+using BBTV6B_HFT_2022231.Models.Entities;
+using BBTV6B_HFT_2022231.Repository;
+using BBTV6B_HFT_2022231.Repository.ModelRepositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace BBTV6B_HFT_2022231.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddTransient<StockDbContext>();
+
+            services.AddTransient<IRepository<Transaction>, TransactionRepository>();
+            services.AddTransient<IRepository<Stock>, StockRepository>();
+            services.AddTransient<IRepository<Exchange>, ExchangeRepository>();
+
+            services.AddTransient<ITransactionLogic, TransactionLogic>();
+            services.AddTransient<IExchangeLogic, ExchangeLogic>();
+            services.AddTransient<IStockLogic, StockLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "StockDB.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StockDB.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context => {
+                var exception = context.Features.Get<IExceptionHandlerPathFeature>().Error;
+                var response = new { error = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
