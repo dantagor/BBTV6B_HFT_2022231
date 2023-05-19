90d98ee0a68a2f6e9cf02bda3b792a580b8ec3ae
dantagor <lendvayzita@gmail.com>
2023-05-19 09:46:12 +0200

    JSClient fájlstruktúra létrehozva

--- BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Endpoint/Startup.cs
+++ BBTV6B_HFT_2022231/BBTV6B_HFT_2022231.Endpoint/Startup.cs
@@ -65,6 +65,12 @@ namespace BBTV6B_HFT_2022231.Endpoint
                 await context.Response.WriteAsJsonAsync(response);
             }));
 
+            app.UseCors(x => x
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins("http://localhost:44324"));
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
