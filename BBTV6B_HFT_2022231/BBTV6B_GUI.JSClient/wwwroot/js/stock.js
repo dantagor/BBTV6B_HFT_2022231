f8618b1a208126931be4811627475b11511c5e7e
dantagor <lendvayzita@gmail.com>
2023-05-19 11:42:36 +0200

    Stock Display implementálva

--- BBTV6B_HFT_2022231/BBTV6B_GUI.JSClient/wwwroot/js/stock.js
+++ BBTV6B_HFT_2022231/BBTV6B_GUI.JSClient/wwwroot/js/stock.js
@@ -1,1 +1,36 @@
-﻿
+﻿let stocks = [];
+let connection;
+let stockIdToUpdate = -1;
+GetStockData();
+//setupSignalR();
+
+// Get & Display Stock Data
+async function GetStockData() {
+    await fetch('http://localhost:33531/stock')
+        .then(x => x.json())
+        .then(y => {
+            stocks = y.$values;
+            console.log(stocks);
+            DisplayStockData();
+        });
+}
+function DisplayStockData() {
+    document.getElementById("stockHolder").innerHTML = "";
+    stocks.forEach(t => {
+        document.getElementById("stockHolder").innerHTML +=
+            `
+            <tr>
+                <th scope="row">${t.ticker}</th>
+                <td>${t.company}</td>
+                <td>${t.exchangeId}</td>
+                <td>${t.dividend}</td>
+                <td>${t.price}</td>
+                <td>
+                    <button type="button" class="btn btn-primary" onclick="ModifyExchange('${t.id}')">Buy</button>
+                    <button type="button" class="btn btn-warning" onclick="ModifyExchange('${t.id}')">Edit</button>
+                    <button type="button" class="btn btn-danger" onclick="DeleteExchange('${t.id}')">Delete</button>
+                </td>
+            </tr>
+            `;
+    });
+}
